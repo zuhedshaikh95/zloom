d36a936e6e0e9ff851839a4bee8ab2ec71d08061
zuhedshaikh95 <zuhed95@gmail.com>
2024-12-31 09:14:50 +0530

    add callback

---
+++ apps/web/src/app/auth/callback/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+const Callback: React.FC<Props> = async ({}) => {
+  // TODO: auto create a workspace and redirect
+
+  return <div>Callback</div>;
+};
+
+export default Callback;
