435f87e49be2375528d37312d58816edd7c9c95f
zuhedshaikh95 <zuhed95@gmail.com>
2025-01-09 09:30:03 +0530

    common export shadcn components

---
+++ apps/web/src/components/ui/index.ts
@@ -0,0 +1,13 @@
+export { Button } from "./button";
+export { Input } from "./input";
+export { Skeleton } from "./skeleton";
+export { Separator } from "./separator";
+export { Label } from "./label";
+export { Switch } from "./switch";
+
+export * as Select from "./select";
+export * as Dialog from "./dialog";
+export * as Avatar from "./avatar";
+export * as Card from "./card";
+export * as Sheet from "./sheet";
+export * as Tabs from "./tabs";
