280d80e182d046bde2658125faa3c9a60c2b2180
zuhedshaikh95 <zuhed95@gmail.com>
2025-01-11 20:39:38 +0530

    common export create workspace

--- apps/web/src/components/global/index.ts
+++ apps/web/src/components/global/index.ts
@@ -4,3 +4,4 @@ export { default as Infobar } from "./infobar";
 export { default as Sidebar } from "./sidebar";
 export { default as WorkspaceSearch } from "./workspace-search";
 export { default as SidebarItem } from "./sidebar-item";
+export { default as CreateWorkspace } from "./create-workspace";
