025eb3dbe5b9dd59c6ddb005abee5da3c32be62e
zuhedshaikh95 <zuhed95@gmail.com>
2025-01-13 23:49:21 +0530

    export redux typed hooks

--- apps/web/src/hooks/index.ts
+++ apps/web/src/hooks/index.ts
@@ -1,2 +1,4 @@
 export { useQueryData } from "./use-query-data";
 export { useMutationData } from "./use-mutation-data";
+export { useAppSelector } from "./use-app-selector";
+export { useAppDispatch } from "./use-app-dispatch";
