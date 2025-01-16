57c56bc0bd766e4190fbb650f2405dcfd3d1fbd6
zuhedshaikh95 <zuhed95@gmail.com>
2025-01-16 07:50:30 +0530

    init clerk-react

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { ClerkProvider } from "@clerk/clerk-react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Configure VITE_CLERK_PUBLISHABLE_KEY in the .env file to start the app!");
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <App />
-  </React.StrictMode>,
-)
+  </ClerkProvider>
+);
 
 // Use contextBridge
-window.ipcRenderer.on('main-process-message', (_event, message) => {
-  console.log(message)
-})
+window.ipcRenderer.on("main-process-message", (_event, message) => {
+  console.log(message);
+});
