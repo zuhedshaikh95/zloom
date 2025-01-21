98a12d9c7f42e053cc27861d6a46210b578f16c4
zuhedshaikh95 <zuhed95@gmail.com>
2025-01-21 22:26:16 +0530

    init axiosInstance, getMediaSources

--- apps/desktop/src/libs/utils.ts
+++ apps/desktop/src/libs/utils.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -6,3 +7,18 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const closeApp = () => window.ipcRenderer.send("close-app");
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_HOST_URL,
+});
+
+export const getMediaSources = async () => {
+  const displays = await window.ipcRenderer.invoke("get-sources");
+  const enumratedDevices = await window.navigator.mediaDevices.enumerateDevices();
+
+  const audioInputs = enumratedDevices.filter((device) => device.kind === "audioinput");
+
+  console.log("getting sources");
+
+  return { displays, audioInputs };
+};
