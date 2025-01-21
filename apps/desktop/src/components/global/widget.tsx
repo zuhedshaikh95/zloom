b285ce23a54e5d8789236d535b712e27083ac2ac
zuhedshaikh95 <zuhed95@gmail.com>
2025-01-21 22:25:49 +0530

    init widget

--- apps/desktop/src/components/global/widget.tsx
+++ apps/desktop/src/components/global/widget.tsx
@@ -1,9 +1,48 @@
-import React from "react";
+import { Loader } from "@/components/global";
+import { useMediaSources } from "@/hooks";
+import { axiosInstance } from "@/libs/utils";
+import { QueryKeysE } from "@/types";
+import { ClerkLoading, useUser } from "@clerk/clerk-react";
+import { useQuery } from "@tanstack/react-query";
+import React, { useEffect } from "react";
 
 type Props = {};
 
 const Widget: React.FC<Props> = ({}) => {
-  return <div>Widget</div>;
+  const { user } = useUser();
+  console.log({ user });
+
+  const { data: profile } = useQuery({
+    queryKey: [QueryKeysE.USER_PROFILE],
+    queryFn: async () => {
+      const response = await axiosInstance.get(`/auth/${user?.id}`);
+
+      return response.data;
+    },
+  });
+
+  const { fetchMediaSources, state } = useMediaSources();
+
+  useEffect(() => {
+    if (user && user.id) {
+    }
+  }, [user]);
+
+  return (
+    <div className="p-5">
+      <ClerkLoading>
+        <div className="h-full flex justify-center items-center">
+          <Loader />
+        </div>
+      </ClerkLoading>
+
+      {/* <SignedIn>
+        {profile ? <MediaConfiguration /> : <div className="w-full h-full flex justify-center items-center">
+          <Loader className="text-white" />
+          </div>}
+      </SignedIn> */}
+    </div>
+  );
 };
 
 export default Widget;
